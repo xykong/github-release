81271ea34ec733ff5fe2606a7f74d85ef6e01937
xykong <xy.kong@gmail.com>
2019-03-25 16:52:14 +0800

    refactor type keys and add to ide dictionaries.

--- github/release.go
+++ github/release.go
@@ -12,33 +12,33 @@ import (
 )
 
 type Release struct {
-	URL             string `json:"url"`
-	AssetsURL       string `json:"assets_url"`
-	UploadURL       string `json:"upload_url"`
-	HTMLURL         string `json:"html_url"`
-	ID              int    `json:"id"`
-	NodeID          string `json:"node_id"`
+	Url             string `json:"url"`
+	AssetsUrl       string `json:"assets_url"`
+	UploadUrl       string `json:"upload_url"`
+	HtmlUrl         string `json:"html_url"`
+	Id              int    `json:"id"`
+	NodeId          string `json:"node_id"`
 	TagName         string `json:"tag_name"`
 	TargetCommitish string `json:"target_commitish"`
 	Name            string `json:"name"`
 	Draft           bool   `json:"draft"`
 	Author          struct {
 		Login             string `json:"login"`
-		ID                int    `json:"id"`
-		NodeID            string `json:"node_id"`
-		AvatarURL         string `json:"avatar_url"`
-		GravatarID        string `json:"gravatar_id"`
-		URL               string `json:"url"`
-		HTMLURL           string `json:"html_url"`
-		FollowersURL      string `json:"followers_url"`
-		FollowingURL      string `json:"following_url"`
-		GistsURL          string `json:"gists_url"`
-		StarredURL        string `json:"starred_url"`
-		SubscriptionsURL  string `json:"subscriptions_url"`
-		OrganizationsURL  string `json:"organizations_url"`
-		ReposURL          string `json:"repos_url"`
-		EventsURL         string `json:"events_url"`
-		ReceivedEventsURL string `json:"received_events_url"`
+		Id                int    `json:"id"`
+		NodeId            string `json:"node_id"`
+		AvatarUrl         string `json:"avatar_url"`
+		GravatarId        string `json:"gravatar_id"`
+		Url               string `json:"url"`
+		HtmlUrl           string `json:"html_url"`
+		FollowersUrl      string `json:"followers_url"`
+		FollowingUrl      string `json:"following_url"`
+		GistsUrl          string `json:"gists_url"`
+		StarredUrl        string `json:"starred_url"`
+		SubscriptionsUrl  string `json:"subscriptions_url"`
+		OrganizationsUrl  string `json:"organizations_url"`
+		ReposUrl          string `json:"repos_url"`
+		EventsUrl         string `json:"events_url"`
+		ReceivedEventsUrl string `json:"received_events_url"`
 		Type              string `json:"type"`
 		SiteAdmin         bool   `json:"site_admin"`
 	} `json:"author"`
@@ -46,28 +46,28 @@ type Release struct {
 	CreatedAt   time.Time `json:"created_at"`
 	PublishedAt time.Time `json:"published_at"`
 	Assets      []struct {
-		URL      string      `json:"url"`
-		ID       int         `json:"id"`
-		NodeID   string      `json:"node_id"`
+		Url      string      `json:"url"`
+		Id       int         `json:"id"`
+		NodeId   string      `json:"node_id"`
 		Name     string      `json:"name"`
 		Label    interface{} `json:"label"`
 		Uploader struct {
 			Login             string `json:"login"`
-			ID                int    `json:"id"`
-			NodeID            string `json:"node_id"`
-			AvatarURL         string `json:"avatar_url"`
-			GravatarID        string `json:"gravatar_id"`
-			URL               string `json:"url"`
-			HTMLURL           string `json:"html_url"`
-			FollowersURL      string `json:"followers_url"`
-			FollowingURL      string `json:"following_url"`
-			GistsURL          string `json:"gists_url"`
-			StarredURL        string `json:"starred_url"`
-			SubscriptionsURL  string `json:"subscriptions_url"`
-			OrganizationsURL  string `json:"organizations_url"`
-			ReposURL          string `json:"repos_url"`
-			EventsURL         string `json:"events_url"`
-			ReceivedEventsURL string `json:"received_events_url"`
+			Id                int    `json:"id"`
+			NodeId            string `json:"node_id"`
+			AvatarUrl         string `json:"avatar_url"`
+			GravatarId        string `json:"gravatar_id"`
+			Url               string `json:"url"`
+			HtmlUrl           string `json:"html_url"`
+			FollowersUrl      string `json:"followers_url"`
+			FollowingUrl      string `json:"following_url"`
+			GistsUrl          string `json:"gists_url"`
+			StarredUrl        string `json:"starred_url"`
+			SubscriptionsUrl  string `json:"subscriptions_url"`
+			OrganizationsUrl  string `json:"organizations_url"`
+			ReposUrl          string `json:"repos_url"`
+			EventsUrl         string `json:"events_url"`
+			ReceivedEventsUrl string `json:"received_events_url"`
 			Type              string `json:"type"`
 			SiteAdmin         bool   `json:"site_admin"`
 		} `json:"uploader"`
@@ -77,10 +77,10 @@ type Release struct {
 		DownloadCount      int       `json:"download_count"`
 		CreatedAt          time.Time `json:"created_at"`
 		UpdatedAt          time.Time `json:"updated_at"`
-		BrowserDownloadURL string    `json:"browser_download_url"`
+		BrowserDownloadUrl string    `json:"browser_download_url"`
 	} `json:"assets"`
-	TarballURL string `json:"tarball_url"`
-	ZipballURL string `json:"zipball_url"`
+	TarballUrl string `json:"tarball_url"`
+	ZipballUrl string `json:"zipball_url"`
 	Body       string `json:"body"`
 }
 
@@ -167,7 +167,7 @@ func ListReleases(owner string, repo string) (Releases, error) {
 	}
 
 	for _, r := range releases {
-		fmt.Printf("%4d    %s\n", r.ID, r.TagName)
+		fmt.Printf("%4d    %s\n", r.Id, r.TagName)
 	}
 
 	return releases, nil
